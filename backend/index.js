d63ae13966991c2a1e9cfff419cebec3a18d1620
Murali R <muralideva443@gmail.com>
2025-05-18 08:49:07 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -26,12 +26,17 @@ const connectDB = async () => {
 dotenv.config();
 
 // CORS configuration
-app.use(cors({
-  origin: ["https://blog-application-task-frontend.onrender.com","http://localhost:5173"],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
-}));
+app.use(
+  cors({
+    origin: [
+      "https://blog-application-task-frontend.onrender.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  })
+);
 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
@@ -39,7 +44,9 @@ app.use(cookieParser());
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: "Something went wrong!", error: err.message });
+  res
+    .status(500)
+    .json({ message: "Something went wrong!", error: err.message });
 });
 
 app.use("/api/auth", authRoute);
@@ -54,7 +61,6 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, fn) => {
     fn(null, req.body.img);
-    // fn(null,"image1.jpg")
   },
 });
 
